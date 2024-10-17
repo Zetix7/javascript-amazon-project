eeb2d1a964006108e84a084b577a571399821645
Zetix7 <billyz7777777@gmail.com>
2024-10-17 13:11:28 +0200

    Delete products from HTML when clicking delete, fix bugs

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,77 +15,79 @@ cart.forEach(cartItem => {
         }
     });
 
-    cartSumaryHTML += `<div class="delivery-date">
-        Delivery date: Tuesday, June 21
-    </div>
-
-    <div class="cart-item-details-grid">
-        <img class="product-image"
-        src="${productFound.image}">
-
-        <div class="cart-item-details">
-        <div class="product-name">
-            ${productFound.name}
-        </div>
-        <div class="product-price">
-            $${formatCurrency(productFound.priceCents)}
-        </div>
-        <div class="product-quantity">
-            <span>
-            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-            </span>
-            <span class="update-quantity-link link-primary">
-            Update
-            </span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${productId}">
-            Delete
-            </span>
-        </div>
+    cartSumaryHTML += `<div class="cart-item-container js-cart-item-container-${productFound.id}">
+        <div class="delivery-date">
+            Delivery date: Tuesday, June 21
         </div>
 
-        <div class="delivery-options">
-        <div class="delivery-options-title">
-            Choose a delivery option:
-        </div>
-        <div class="delivery-option">
-            <input type="radio" checked
-            class="delivery-option-input"
-            name="delivery-option-${productId}">
-            <div>
-            <div class="delivery-option-date">
-                Tuesday, June 21
+        <div class="cart-item-details-grid">
+            <img class="product-image"
+            src="${productFound.image}">
+
+            <div class="cart-item-details">
+            <div class="product-name">
+                ${productFound.name}
             </div>
-            <div class="delivery-option-price">
-                FREE Shipping
+            <div class="product-price">
+                $${formatCurrency(productFound.priceCents)}
             </div>
-            </div>
-        </div>
-        <div class="delivery-option">
-            <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${productId}">
-            <div>
-            <div class="delivery-option-date">
-                Wednesday, June 15
-            </div>
-            <div class="delivery-option-price">
-                $4.99 - Shipping
+            <div class="product-quantity">
+                <span>
+                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                </span>
+                <span class="update-quantity-link link-primary">
+                Update
+                </span>
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${productFound.id}">
+                Delete
+                </span>
             </div>
             </div>
-        </div>
-        <div class="delivery-option">
-            <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${productId}">
-            <div>
-            <div class="delivery-option-date">
-                Monday, June 13
+
+            <div class="delivery-options">
+            <div class="delivery-options-title">
+                Choose a delivery option:
             </div>
-            <div class="delivery-option-price">
-                $9.99 - Shipping
+            <div class="delivery-option">
+                <input type="radio" checked
+                class="delivery-option-input"
+                name="delivery-option-${productFound.id}">
+                <div>
+                <div class="delivery-option-date">
+                    Tuesday, June 21
+                </div>
+                <div class="delivery-option-price">
+                    FREE Shipping
+                </div>
+                </div>
+            </div>
+            <div class="delivery-option">
+                <input type="radio"
+                class="delivery-option-input"
+                name="delivery-option-${productFound.id}">
+                <div>
+                <div class="delivery-option-date">
+                    Wednesday, June 15
+                </div>
+                <div class="delivery-option-price">
+                    $4.99 - Shipping
+                </div>
+                </div>
+            </div>
+            <div class="delivery-option">
+                <input type="radio"
+                class="delivery-option-input"
+                name="delivery-option-${productFound.id}">
+                <div>
+                <div class="delivery-option-date">
+                    Monday, June 13
+                </div>
+                <div class="delivery-option-price">
+                    $9.99 - Shipping
+                </div>
+                </div>
             </div>
             </div>
-        </div>
         </div>
     </div>`;
 });
@@ -95,5 +97,10 @@ document.querySelectorAll('.js-delete-link').forEach(link => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+        console.log(productId);
+        console.log(cart);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     });
 });
