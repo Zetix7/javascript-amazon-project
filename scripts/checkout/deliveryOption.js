2969b0c4548ef75f22c906eed7e30920ab258395
Zetix7 <billyz7777777@gmail.com>
2024-10-21 16:38:01 +0200

    Delivery date skips weekend

--- scripts/checkout/deliveryOption.js
+++ scripts/checkout/deliveryOption.js
@@ -2,7 +2,18 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 export function calculateDeliveryDate(deliveryOption){
     const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+    let deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+
+    while(isWeekend(deliveryDate)){
+        deliveryDate = deliveryDate.add(1, 'day');
+    }
+
     const dateString = deliveryDate.format('dddd, MMMM D');
+
     return dateString;
+}
+
+function isWeekend(deliveryDate){
+    const dayOfWeek = deliveryDate.format('dddd');
+    return (dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday');
 }
