e1e625e87f19c0de12a4606d8eb8c57451a661d9
Zetix7 <billyz7777777@gmail.com>
2024-10-23 15:20:33 +0200

    Update Jasmin version, Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,19 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [{
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-    deliveryOptionId: '1'
-},
-{
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-    deliveryOptionId: '2'
-}];
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+    cart = JSON.parse(localStorage.getItem('cart')) || [{
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: '1'
+    },
+    {
+        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: '2'
+    }];
+}
 
 function saveToCart(){
     localStorage.setItem('cart', JSON.stringify(cart));
@@ -16,6 +22,7 @@ function saveToCart(){
 export function addToCart(productId){
     const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
     const quantity = Number(quantitySelector.value);
+    // const quantity = 1;
     
     let foundProduct;
 
