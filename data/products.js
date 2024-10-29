a2229ae2f24f98ba1f3822f29438f6b7704c4996
Zetix7 <billyz7777777@gmail.com>
2024-10-29 10:43:44 +0100

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -68,6 +68,31 @@ export class Appliance extends Product{
   }
 }
 
+export let products = [];
+
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+  
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map(productDetails => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      else if(productDetails.type === 'appliance'){
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+
+    fun();
+  });
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -739,3 +764,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
